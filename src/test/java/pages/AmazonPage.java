b0c17052e805ad88541ad61e3deafd35b80df0d5
mu5tafacool <mustafakulf1@gmail.com>
2022-05-30 00:26:07 +0300

    30 05 22 00:30

--- src/test/java/pages/AmazonPage.java
+++ src/test/java/pages/AmazonPage.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-        public class AmazonPage { //parametresiz conts. cagirilan constructurda Driver clasindaki driver bu driver dir.gecerli hale getirmek icin
+        public class AmazonPage { //parametresiz conts. cagirilan constructurda
+            // Driver clasindaki driver bu driver dir.gecerli hale getirmek icin
             public AmazonPage(){
                 PageFactory.initElements(Driver.getDriver(),this);
             }
