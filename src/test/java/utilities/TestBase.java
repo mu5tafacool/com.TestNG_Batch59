adce60f7a69f22ed19d573afd96b7bc50fc33f74
mu5tafacool <mustafakulf1@gmail.com>
2022-05-30 00:26:20 +0300

    30 05 22 00:30

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,7 +1,6 @@
 package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
