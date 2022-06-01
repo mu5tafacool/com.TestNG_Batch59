d8c7c9a8ec2a6f81010ce94dec63753398e797b1
mu5tafacool <mustafakulf1@gmail.com>
2022-06-02 00:15:16 +0300

    2 06 22 00:25

--- src/test/java/tests/day18_pom/C01_ConfigReader.java
+++ src/test/java/tests/day18_pom/C01_ConfigReader.java
@@ -1,8 +1,13 @@
 package tests.day18_pom;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.testng.annotations.Test;
+import pages.AmazonPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class C01_ConfigReader {
 
@@ -12,5 +17,8 @@ public class C01_ConfigReader {
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
         Driver.getDriver().get(ConfigReader.getProperty("facebookUrl"));
         Driver.closeDriver();
+
+
     }
+
 }
